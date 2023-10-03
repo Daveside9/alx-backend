341fc24f2342f1472a6e395d7913bfe4a17e075a
Joel David <106955759+Daveside9@users.noreply.github.com>
2023-10-03 00:21:56 -0700

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
 """
-This_module_defines_a_basic_Flask_app.
+Flask app setup
 """
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    """
-    Renders_the_index.html_template.
-    """
-    return render_template(
-            '0-index.html', title='Welcome_to_Holberton',
-            header='Hello world'
-    )
+@app.route("/")
+def home():
+    """ display_home_route"""
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+    app.run(debug=True)
